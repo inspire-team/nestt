48f2bc53be50c491195e1b4945fd393186aa834a
Renee Campanilla <renee@inspire.co.uk>
2016-12-07 15:22:14 +0800

    Made contact page dynamic.

--- wp-content/themes/nestt-theme/templates/homepage-contact.php
+++ wp-content/themes/nestt-theme/templates/homepage-contact.php
@@ -1,37 +1,20 @@
-
+<?php $data = get_page_by_path('contact'); ?>
 <!--contact section-->
 <section id="contact" class="section_8 inner-section">
     <div class="contact_div">
         <h1 class="page-counter">07</h1>
         <div class="container">
             <div class="inner-content">
-                <h2 class="heading"><span>Contact</span></h2>
+                <h2 class="heading"><span><?= $data->post_title; ?></span></h2>
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="contact_section">
-                                <div class="contact_person text-left" id="contact_outer_container">
-                                    <div class="contact_pic inline-block"><img src="<?= get_bloginfo('template_url') . '/assets/images/michael_pic.png'; ?>" alt="Michael Rosemann"></div>
-                                    <div class="contact_info inline-block middle-div" id="contact_inner_container">
-                                        <div class="contact_name">Prof Michael Rosemann</div>
-                                        <div class="contact_position">Executive Director Corporate Engagement</div>
-                                        <div class="contact_location">Queensland University of Technology</div>
-                                    </div>
-                                </div>
-                                <div class="contact_data">
-                                    <p class="no-bottom text-left contact_data_header">Contact us, if you are interested</p>
-                                    <ul>
-                                        <li><span class="long_dash">&mdash;</span><p class="contact_conditions">in participating in our NESTT courses, either as a participant in our open enrollment course or if you are interested in organising an in-house event</p></li>
-                                        <li><span class="long_dash">&mdash;</span><p class="contact_conditions">in becoming a delivery partner for the NESTT, in Australia or globally</p></li>
-                                        <li><span class="long_dash">&mdash;</span><p class="contact_conditions">in conducting a NESTT redesign initiative within your organisation</p></li>
-                                        <li><span class="long_dash">&mdash;</span><p class="contact_conditions">in conducting research in the area of rapid redesign approaches</p></li>
-                                    </ul>
-                                </div>
-                                <div class="text-left contact_email">
-                                    <p>Email: nestt@qut.edu.au</p>
-                                </div>
+                                <?php
+                                echo apply_filters('the_content', $data->post_content);
+                                ?>
                                 <div class="contact_subscribe">
                                     <p class="contact_subscribe_header">SUBSCRIBE<span class="contact_join">JOIN OUR MAILING LIST</span></p>
-                                    <?php echo do_shortcode("[ninja_form id=1]");?>
+                                    <?php echo do_shortcode("[ninja_form id=2]");?>
                                 </div>
                             </div>
                         </div>
