788fd33e78895aed7388cb97de2688d90a90bd6c
Jake Monton <jake@inspire.co.uk>
2016-12-07 11:41:54 +0800

    added wordpress importer. updated shell script

--- system/scripts/update_wp_content_dir.sh
+++ system/scripts/update_wp_content_dir.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-cd /www/sites/ozlotteries.lan; composer install;
+cd /www/sites/nestt.lan; #composer install;
 
-sed -i "/DB_COLLATE/a defined(\'WP_CONTENT_DIR\') or define( \'WP_CONTENT_DIR', dirname(__FILE__) . '/../wp-content' );" /www/sites/ozlotteries.lan/wp/wp-config.php
-sed -i "/WP_CONTENT_DIR/a \$content_url = (!empty(\$_SERVER\['HTTPS'\]) \&\& \$_SERVER\['HTTPS'] !== 'off') ? 'https://' : 'http://';" /www/sites/ozlotteries.lan/wp/wp-config.php
-sed -i "/\$content/a defined('WP_CONTENT_URL') or define( 'WP_CONTENT_URL', \$content_url .  \$_SERVER\['HTTP_HOST'].'/wp-content' );" /www/sites/ozlotteries.lan/wp/wp-config.php
+sed -i "/DB_COLLATE/a defined(\'WP_CONTENT_DIR\') or define( \'WP_CONTENT_DIR', dirname(__FILE__) . '/../wp-content' );" /www/sites/nestt.lan/wp/wp-config.php
+sed -i "/WP_CONTENT_DIR/a \$content_url = (!empty(\$_SERVER\['HTTPS'\]) \&\& \$_SERVER\['HTTPS'] !== 'off') ? 'https://' : 'http://';" /www/sites/nestt.lan/wp/wp-config.php
+sed -i "/\$content/a defined('WP_CONTENT_URL') or define( 'WP_CONTENT_URL', \$content_url .  \$_SERVER\['HTTP_HOST'].'/wp-content' );" /www/sites/nestt.lan/wp/wp-config.php
